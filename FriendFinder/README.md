a0cbf898c960a8464c5aedc8ffbcd4a4648c7f03
Alex H <hampton.alex10@gmail.com>
2020-02-28 12:24:37 -0700

    Routes working, made some fun changes

---
+++ FriendFinder/README.md
@@ -0,0 +1,14 @@
+# FriendFinder
+
+## Heroku Link:
+https://mighty-taiga-71667.herokuapp.com/ * Not currently working * 
+
+## Assignment Overview
+FriendFinder is a compatability-based application -- basically a dating app. This full-stack site will take in results from  users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+## Tools Used
+* Node.js 
+* Express.js
+* HTML
+* Javascript
+* J Query 
---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,39 @@
+var friendList = [
+  {
+    fname: 'Toby',
+    lname: 'Flenderson',
+    profilePic: 'https://img.nbc.com/sites/nbcunbc/files/scet/photos/22/1964/NUP_130416_0454.JPG?impolicy=nbc_com&imwidth=1080&imdensity=1',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  
+},
+{
+fname: 'Michael',
+lname: 'Scott',
+profilePic: "https://img.nbc.com/sites/nbcunbc/files/scet/photos/22/3805/194_michael.jpg?impolicy=nbc_com&imwidth=1080&imdensity=1",
+scores: [
+"5",
+"3",
+"5",
+"2",
+"5",
+"3",
+"2",
+"5",
+"3",
+"2"
+]
+}
+]
+
+module.exports = friendList;
